f0348673f16e914b22b544bf400a53a64ab5744c
wmcg0001 <wmcg0001@student.monash.edu>
2022-09-22 09:20:40 +1000

    feature(diffusion): generate images for album covers

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import logging
 from gui import Gui
+from huggingface_hub import notebook_login
+import torch
+import constant
 
 # Contains information for auth
 import secret
 
 if __name__ == "__main__":
+    notebook_login()
+    print(torch.cuda.is_available())
+    print(torch.cuda.memory_summary(device=None, abbreviated=False))
     # Set scope
     scope = "user-library-read playlist-modify-public ugc-image-upload"
 
@@ -19,5 +25,5 @@ if __name__ == "__main__":
     except spotipy.SpotifyException as e:
         logging.log("Authentication Failed")
 
-    gui = Gui(sp, 640, 640)
+    gui = Gui(sp, constant.png_px_size, constant.png_px_size)
     gui.start()
