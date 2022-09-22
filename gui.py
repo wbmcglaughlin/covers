991eaa20e2b5e6f84a8cfd9dcf160e87a9672772
wmcg0001 <wmcg0001@student.monash.edu>
2022-09-22 10:01:32 +1000

    feature(gui): select specific playlist

--- gui.py
+++ gui.py
@@ -17,18 +17,36 @@ class Gui:
         self.height = height
 
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
-        self.canvas.pack()
+        self.canvas.pack(fill='both', expand=1)
 
         self.cover = Cover(sp)
+        self.index_val = 0
+        self.index = StringVar()
 
     def start(self):
         buff = self.cover.generate_image(0, 48, 209)
         img = ImageTk.PhotoImage(Image.open(buff))
 
         self.canvas.create_image(0, 0, anchor=NW, image=img)
-        self.cover.generate_images(48, 209)
 
+        left_button = Button(master=self.root, text="<<", command=self.decrement_index)
+        left_button.pack()
         button = Button(master=self.root, text="Apply Cover Arts", command=self.cover.set_image)
         button.pack()
-
+        right_button = Button(master=self.root, text=">>", command=self.increment_index)
+        right_button.pack()
+        text = Label(self.root, textvariable=self.index)
+        text.pack()
         self.root.mainloop()
+
+    def decrement_index(self):
+        self.index_val -= 1
+        self.index_val = max(self.index_val, 0)
+        self.index.set(str(self.index_val))
+
+    def increment_index(self):
+        self.index_val += 1
+        self.index.set(str(self.index_val))
+
+    def get_image(self):
+        self.cover.get_image(self.index_val)
