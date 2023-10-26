f8d6c08d8ddee80b02c2eba5f9a953a65d29f7ba
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-10-26 21:38:43 +1100

    feature: use appdirs

--- configurations.py
+++ configurations.py
@@ -1,27 +1,28 @@
 import os
+import appdirs
+import pathlib
 import configparser
+from constant import app_author, app_name
 
 
 def get_persistent_data_path():
-    path = os.getenv('APPDATA') + "\\PrettyPlaylists"
+    path = pathlib.Path(appdirs.user_data_dir(appauthor=app_author, appname=app_name))
     if not os.path.exists(path):
         os.mkdir(path)
 
     return path
-
 
 def make_configurations_folder():
     config_folder_path = get_configurations_folder()
     if not os.path.exists(config_folder_path):
         os.mkdir(config_folder_path)
 
-
 def get_configurations_folder():
-    return get_persistent_data_path() + '\\configurations'
+    return get_persistent_data_path() / 'configurations'
 
 
 def get_configurations_file():
-    return get_configurations_folder() + '\\user_configurations.ini'
+    return get_configurations_folder() / 'user_configurations.ini'
 
 
 def make_configurations_file():
--- diffusion.py
+++ diffusion.py
@@ -7,8 +7,11 @@ import constant
 def get_diffusion_image(string):
     experimental_pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16",
                                                                 torch_dtype=torch.float16, use_auth_token=True)
-    experimental_pipe = experimental_pipe.to("cuda")
-
+    try:
+        experimental_pipe = experimental_pipe.to("cuda")
+    except Exception as e:
+        experimental_pipe = experimental_pipe.to("cpu")
+        
     with autocast("cuda"):
         image_1 = experimental_pipe(string, height=constant.png_px_size, width=constant.png_px_size).images[0]
 
