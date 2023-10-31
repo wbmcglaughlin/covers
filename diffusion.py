507961989fe03d2d6cc2fb3aa30f9bc65f719f27
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-10-31 20:49:17 +1100

    fix: remove unused import

--- diffusion.py
+++ diffusion.py
@@ -1,7 +1,6 @@
 import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
-from PIL import Image
 import constant
 
 def get_diffusion_image(string):
@@ -13,6 +12,6 @@ def get_diffusion_image(string):
         experimental_pipe = experimental_pipe.to("cpu")
         
     with autocast("cuda"):
-        image_1 = experimental_pipe(string, height=constant.png_px_size, width=constant.png_px_size).images[0]
+        image = experimental_pipe(string, height=constant.png_px_size, width=constant.png_px_size).images[0]
 
-    return image_1
+    return image
