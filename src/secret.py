49bcb7227290b4c6d556724a5fe9e7adbb0ae6e6
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-11-12 11:40:12 +1100

    feat: update token

--- src/secret.py
+++ src/secret.py
@@ -1,3 +1,3 @@
 client_id = "03894c7559ef43a0b37974267d05c327"
-client_secret = "81501ce586fb4983b3aa383b57f3340f"
+client_secret = "52232f264060435ab4b16d0b17f2b4fc"
 redirect_uri = "http://localhost:8888/callback"
